bb7db1c215651760b3571107f7af965934ddd8a6
Armin0078 <molimol2689@gmail.com>
2024-06-06 15:03:59 +0330

    Add project files.

---
+++ MobileSensors/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MobileSensors
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ MobileSensors/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileSensors
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
